6a7485fd8aa1d43334d6825cbfe5b191c402fec1
ahhampton <45833310+ahhampton@users.noreply.github.com>
2019-09-11 15:09:49 -0400

    Update si506_lab_02.py

--- si506_lab_02.py
+++ si506_lab_02.py
@@ -1,8 +1,17 @@
 print('Lab Exercise 02 \n')
-course_number = 'SI 506'
-course_name = ': Programming I'
-full_course_name = course_number + course_name
-print(full_course_name)
+course_name = 'SI 506'
+course_name = 'SI 506' + ': Programming I'
+print(course_name)
 #print(len(full_course_name))
 num_chars = 21
 print(''.join(['num_chars = ', str(num_chars), '\n']))
+
+#ALTERNATE:
+#print('Lab Exercise 02 \n')
+#course_number = 'SI 506'
+#course_name = ': Programming I'
+#full_course_name = course_number + course_name
+#print(full_course_name)
+##print(len(full_course_name))
+#num_chars = 21
+#print(''.join(['num_chars = ', str(num_chars), '\n']))
