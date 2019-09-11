6e529d9f77409e9e7a3a4280e9e141a5a390d82b
ahhampton <45833310+ahhampton@users.noreply.github.com>
2019-09-11 14:53:42 -0400

    Create si506_lab_02.py

---
+++ si506_lab_02.py
@@ -0,0 +1,8 @@
+print('Lab Exercise 02 \n')
+course_number = 'SI 506'
+course_name = ': Programming I'
+full_course_name = course_number + course_name
+print(full_course_name)
+print(len(full_course_name))
+num_chars = 21
+print(''.join(['num_chars = ', str(num_chars), '\n']))
